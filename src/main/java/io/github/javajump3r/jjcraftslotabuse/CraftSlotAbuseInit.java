722153441ebf2edc7542a76fe31b63fd1e8df0b3
ipz233_vvv <jumpergooog@gmail.com>
2023-12-29 12:13:55 +0200

    ff

--- src/main/java/io/github/javajump3r/jjcraftslotabuse/CraftSlotAbuseInit.java
+++ src/main/java/io/github/javajump3r/jjcraftslotabuse/CraftSlotAbuseInit.java
@@ -1,13 +1,17 @@
 package io.github.javajump3r.jjcraftslotabuse;
 
+import io.github.javajump3r.autocfg.CustomCategory;
+import io.github.javajumper.lavajumper.LavaJumper;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
 public class CraftSlotAbuseInit implements ClientModInitializer {
+
 	private static boolean isLavaJumperLoaded;
 	public static boolean abuseEnabled(){
 		if(isLavaJumperLoaded){
 			return CraftSlotAbuseConfig.enabled;
+			LavaJumper.LavaJumperConfig.addClassToConfig();
 		}
 		return true;
 	}
